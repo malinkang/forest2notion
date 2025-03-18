8a38e5bc5271f51b87a07028eaaaa9f7ccf801eb
malinkang <malinkang@adsconflux.com>
2025-03-18 11:59:14 +0800

    fix heatmap

--- scripts/update_heatmap.py
+++ scripts/update_heatmap.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     notion_helper = NotionHelper()
     image_file = get_file()
     if image_file:
-        image_url = upload_image(f"heatmap/{os.getenv('REPOSITORY').split('/')[0]}",image_file,f"./OUT_FOLDER/{image_file}")
+        image_url = f"https://raw.githubusercontent.com/{os.getenv('REPOSITORY')}/{os.getenv('REF').split('/')[-1]}/OUT_FOLDER/{image_file}"
         heatmap_url = f"https://heatmap.malinkang.com/?image={image_url}"
         notion_helper.update_heatmap(
                 block_id=notion_helper.heatmap_block_id, url=heatmap_url
